80f5919a68d076b596bfc468788a0260ec44cd71
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-19 14:03:29 -0400

    Adds constructor for required fields

--- src/main/java/com/beta/Person.java
+++ src/main/java/com/beta/Person.java
@@ -4,4 +4,8 @@ public class Person {
     private final String firstName;
     private final String lastName;
 
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
