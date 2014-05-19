1cd1b05ad83ce474a0d827bfbd452c00b1605ffc
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-19 13:59:59 -0400

    Adds necessary constructor

--- src/main/java/com/beta/BolderingRoute.java
+++ src/main/java/com/beta/BolderingRoute.java
@@ -1,6 +1,19 @@
 package com.beta;
 
+import org.joda.time.LocalDate;
+
+import java.awt.*;
+
 public class BolderingRoute extends Route {
     private final BoulderingGrade difficulty;
 
+    public BolderingRoute(RouteId lookup,
+                          String routeName,
+                          LocalDate createdAt,
+                          RouteSetter creator,
+                          Color tape,
+                          BoulderingGrade difficulty) {
+        super(lookup, routeName, createdAt, creator, tape);
+        this.difficulty = difficulty;
+    }
 }
