2d5ab75c6b61a508ecd2814ff20bdfec2c939b3b
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-19 15:18:51 -0400

    Formatting

--- src/main/java/com/beta/BetaUser.java
+++ src/main/java/com/beta/BetaUser.java
@@ -5,7 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class BetaUser {
     @JsonProperty
     public final int id;
-    @JsonProperty public final String name;
+    @JsonProperty
+    public final String name;
 
     public BetaUser(int id, String name) {
         this.id = id;
