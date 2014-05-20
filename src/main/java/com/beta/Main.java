aebef9e3a38950f535feb7cd2fdecbcf4fc44fc1
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-20 18:03:34 -0400

    Cleans out hello-world templates

--- src/main/java/com/beta/Main.java
+++ src/main/java/com/beta/Main.java
@@ -43,89 +43,13 @@ public class Main extends Application<Main.BetaConfig> {
         // Manually add JMX reporting (Dropwizard regression)
         JmxReporter.forRegistry(env.metrics()).build().start();
 
-        env.jersey().register(new HelloWorldResource(cfg));
-
-        Feign.Builder feignBuilder = Feign.builder()
-                .contract(new JAXRSModule.JAXRSContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder());
-        env.jersey().register(new ConsumerResource(feignBuilder));
-
         new ApiV1().run(cfg, env);
     }
 
     // YAML Configuration
     public static class BetaConfig extends Configuration {
-        @JsonProperty private @NotEmpty String template;
-        @JsonProperty private @NotEmpty String defaultName;
         @Valid @NotNull @JsonProperty private DataSourceFactory database = new DataSourceFactory();
 
         public DataSourceFactory getDataSourceFactory() { return database; }
-        public String getTemplate()    { return template; }
-        public String getDefaultName() { return defaultName; }
-    }
-
-    // The actual service
-    @Path("/hello-world")
-    @Produces(MediaType.APPLICATION_JSON)
-    public static class HelloWorldResource {
-        private final AtomicLong counter = new AtomicLong();
-        private final String template;
-        private final String defaultName;
-
-        public HelloWorldResource(BetaConfig configuration) {
-            this.template = configuration.getTemplate();
-            this.defaultName = configuration.getDefaultName();
-        }
-
-        @Timed
-        @GET
-        public Saying sayHello(@QueryParam("name") Optional<String> name) throws InterruptedException {
-            final String value = String.format(template, name.or(defaultName));
-            Thread.sleep(ThreadLocalRandom.current().nextInt(10, 500));
-            return new Saying(counter.incrementAndGet(), value);
-        }
-    }
-
-    @Path("/consumer")
-    @Produces(MediaType.TEXT_PLAIN)
-    public static class ConsumerResource {
-        private final HelloWorldAPI helloWorld;
-
-        public ConsumerResource(Feign.Builder feignBuilder) {
-            this.helloWorld = feignBuilder.target(HelloWorldAPI.class, "http://localhost:8080");
-        }
-
-        @Timed
-        @GET
-        public String consume() {
-            Saying saying = helloWorld.hi("consumer");
-            return String.format("The service is saying: %s (id: %d)",  saying.getContent(), saying.getId());
-        }
-    }
-
-    interface HelloWorldAPI {
-        @GET @Path("/hello-world")
-        Saying hi(@QueryParam("name") String name);
-
-        @GET @Path("/hello-world")
-        Saying hi();
-    }
-
-    // JSON (immutable!) payload
-    public static class Saying {
-        private long id;
-        private @Length(max = 10) String content;
-
-        public Saying(long id, String content) {
-            this.id = id;
-            this.content = content;
-        }
-
-        // required for deserialization
-        public Saying() {}
-
-        @JsonProperty public long getId() { return id; }
-        @JsonProperty public String getContent() { return content; }
     }
 }
