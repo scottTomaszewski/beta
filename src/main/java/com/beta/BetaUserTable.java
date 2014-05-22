67114a1b81737b9d7a8869d3f3a8d116fa2e0659
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-22 11:58:53 -0400

    Adds enum for BetaUser table for ease of use

---
+++ src/main/java/com/beta/BetaUserTable.java
@@ -0,0 +1,48 @@
+package com.beta;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import java.util.Arrays;
+
+public enum BetaUserTable {
+    ID("id", "int", "primary key auto_increment"),
+    EMAIL("email", "varchar(100)"),
+    PASSWORD("password", "varchar(128)"),
+    SALT("salt", "varchar(128)"),
+    FIRST_NAME("firstName", "varchar(100)"),
+    LAST_NAME("lastName", "varchar(100)"),
+    PROFILE_PICTURE_ABSOLUTE_PATH("profilePictureAbsolutePath", "varchar(1024)");
+
+    public static final String TABLE_NAME = "beta_user";
+
+    private final String columnName;
+    private final String type;
+    private final String extras;
+
+    BetaUserTable(String columnName, String type) {
+        this(columnName, type, "");
+    }
+
+    BetaUserTable(String columnName, String type, String extras) {
+        this.columnName = columnName;
+        this.type = type;
+        this.extras = extras;
+    }
+
+    public static String creation() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("create table ");
+        sb.append(TABLE_NAME);
+        sb.append(" (");
+        Arrays.stream(BetaUserTable.values()).forEach(column -> sb.append(column.create()));
+        // remove last ", "
+        sb.delete(sb.length()-2, sb.length()-1);
+        sb.append(")");
+        return sb.toString();
+    }
+
+    @VisibleForTesting
+    String create() {
+        return String.format("%s %s%s, ", columnName, type, extras.isEmpty() ? "": " " + extras);
+    }
+}
