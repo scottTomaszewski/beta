d7d90290fa12c462237a3cdc0ae3d41ba6505c3f
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-27 15:56:27 -0400

    Basic tests for serialization of Routes

--- src/test/java/com/beta/api/v1/NewBetaUserAcceptanceTest.java
+++ src/test/java/com/beta/api/v1/NewBetaUserAcceptanceTest.java
@@ -1,4 +1,33 @@
 package com.beta.api.v1;
 
+import com.beta.Main;
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+
+
 public class NewBetaUserAcceptanceTest {
+    @ClassRule
+    public static final DropwizardAppRule<Main.BetaConfig> RULE =
+            new DropwizardAppRule<>(Main.class, "beta.yaml");
+
+    @Test
+    public void loginHandlerRedirectsAfterPost() throws IOException {
+        Client client = new Client();
+
+        ClientResponse response = client.resource(
+                String.format("http://localhost:%d/api/v1/profiles/add", RULE.getLocalPort()))
+                .post(ClientResponse.class, fixture("fixtures/NewBetaUser.json"));
+
+        System.out.println(CharStreams.toString(new InputStreamReader(response.getEntityInputStream(), Charsets.UTF_8)));
+    }
 }
