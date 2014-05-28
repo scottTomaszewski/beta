34841cf65d5ccf74e11db44c5db4b5deaa435019
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-27 23:01:29 -0400

    Corrects typo

--- src/main/java/com/beta/api/v1/ApiV1.java
+++ src/main/java/com/beta/api/v1/ApiV1.java
@@ -13,6 +13,6 @@ public class ApiV1 implements Api {
 
         final DBI dbi = new DBIFactory().build(env, cfg.getDataSourceFactory(), "db");
         env.jersey().register(new Profiles(dbi.onDemand(ProfilesDAO.class), cfg.getProfilePicturesAbsolutePath()));
-        env.jersey().register(new Routes(dbi.onDemand(RoutesDAO.class);));
+        env.jersey().register(new Routes(dbi.onDemand(RoutesDAO.class)));
     }
 }
