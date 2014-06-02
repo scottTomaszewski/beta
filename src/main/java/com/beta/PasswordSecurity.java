aba03b9aee4c43d5d58e3351f38020dfa14fd120
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-06-02 10:27:28 -0400

    Adds helper for securing a password

---
+++ src/main/java/com/beta/PasswordSecurity.java
@@ -0,0 +1,26 @@
+package com.beta;
+
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+
+import java.nio.CharBuffer;
+import java.util.Arrays;
+
+public class PasswordSecurity {
+    private final char[] plaintext;
+    private final CharBuffer salt;
+
+    public PasswordSecurity(char[] plaintext, CharBuffer salt) {
+        this.plaintext = plaintext;
+        this.salt = salt;
+    }
+
+    public String secure() {
+        Hasher h = Hashing.sha512().newHasher();
+        salt.chars().forEach(h::putInt);
+        CharBuffer.wrap(plaintext).chars().forEach(h::putInt);
+        String hashed = h.hash().toString();
+        Arrays.fill(plaintext, 'a');
+        return hashed;
+    }
+}
