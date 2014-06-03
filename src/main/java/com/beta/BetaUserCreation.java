bb5d0ca92b8010b07b1237b37f4224b3967cc90f
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-06-03 00:02:45 -0400

    Cleans up betaUserCreation to simplify

--- src/main/java/com/beta/BetaUserCreation.java
+++ src/main/java/com/beta/BetaUserCreation.java
@@ -2,31 +2,22 @@ package com.beta;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Optional;
-import com.google.common.hash.Hasher;
-import com.google.common.hash.Hashing;
 
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Arrays;
 
-// TODO - cleanup
 public final class BetaUserCreation {
-    @JsonProperty
-    private String email;
-    @JsonProperty
-    private char[] plainTextPassword;
+    public final String email;
+    public final char[] plainTextPassword;
 
     // Lazy
     private String salt;
 
-    // needed for Jackson
-    BetaUserCreation() {}
-
     @VisibleForTesting
-    public BetaUserCreation(String email, char[] plainTextPassword) {
+    public BetaUserCreation(@JsonProperty("email") String email,
+                            @JsonProperty("plainTextPassword") char[] plainTextPassword) {
         this.email = email;
         this.plainTextPassword = plainTextPassword;
     }
@@ -38,7 +29,6 @@ public final class BetaUserCreation {
     public String salt() { return getSalt(); }
 
     public String hashAndSaltPasswordThenClear() {
-        System.out.println(getSalt());
         String hash = new PasswordSecurity(plainTextPassword, getSalt()).secure();
         Arrays.fill(plainTextPassword, 'a');
         return hash;
